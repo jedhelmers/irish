42cd660c6829b85e4a3961df6f17cc7e5b141a71
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 14:54:13 -0400

    more bullshit

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ __all__ = ('celery_app',)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+DISABLE_COLLECTSTATIC = 1
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
