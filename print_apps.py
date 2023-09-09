04f18086706e7c8be2ed67c7d71a0beb5f6e0e26
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 14:13:18 -0400

    print stuff

--- print_apps.py
+++ print_apps.py
@@ -1,5 +1,15 @@
+import os
+import django
 from django.conf import settings
 
+# Set the Django settings module environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')  # Replace 'my_project.settings' with the path to your settings
+
+# Setup Django
+django.setup()
+
+
 print('\nBUTTS')
+print(settings)
 print(','.join(settings.INSTALLED_APPS))
 print('BUTTS END\n')
