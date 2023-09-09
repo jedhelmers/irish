5be4a2df2be703c10cde59ba45e316780bc78220
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 15:31:03 -0400

    more stuff

--- my_app/models.py
+++ my_app/models.py
@@ -8,6 +8,13 @@ class Tags(models.Model):
     def __str__(self):
         return self.tag
 
+class Test(models.Model):
+    creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.creation
+
+
 class UserQueries(models.Model):
     creation = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
