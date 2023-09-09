601bbc3d44815f6ed38a91a81542d5334fea1b08
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 10:05:46 -0400

    grr

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+/app/wait-for-it.sh db:5432 --timeout=30
+
 # wait-for-postgres.sh
 until PGPASSWORD=mypassword psql -h "db" -U "myuser" -d "mydatabase" -c '\l'; do
   echo "Postgres is unavailable - sleeping..."
   sleep 1
 done
+
 
 # Run migrations
 python manage.py migrate
