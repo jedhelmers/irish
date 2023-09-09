48f6918a307fe2cad7bd4a830e5a06126c6694a3
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 19:56:24 -0400

    stuff

--- backend/settings.py
+++ backend/settings.py
@@ -134,14 +134,23 @@ CACHES = {
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'myuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
         'USER': 'myuser',
         'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
     }
 }
 
