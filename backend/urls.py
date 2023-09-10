f34c46baae59daf36c5f6a2a240552c47c2c4cfd
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 23:30:00 -0400

    jesus.

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('metrics/', include('django_prometheus.urls')),
-    # path('', include('my_app.urls')),
+    path('api/', include('my_app.urls')),
     path('', views.CustomTemplateView.as_view(), name='home'),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
--- my_app/urls.py
+++ my_app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from my_app import views
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('api/translate/', views.translate_view, name='translate'),
-    path('api/add_tags/<int:query_id>/', views.add_tags_to_userquery, name='add_tags_to_userquery'),
-    path('api/remove_tags/<int:query_id>/', views.remove_tags_from_userquery, name='remove_tags_from_userquery'),
-    path('api/remove_query/<int:query_id>/', views.remove_userquery, name='remove_userquery'),
-    path('api/get_queries/<int:user_id>/', views.get_userqueries, name='get_userqueries'),
-    path('api/tags/', views.get_all_tags, name='get_all_tags'),
-    path('api/submit_guess/', views.submit_guess, name='submit_guess'),
+    path('translate/', views.translate_view, name='translate'),
+    path('add_tags/<int:query_id>/', views.add_tags_to_userquery, name='add_tags_to_userquery'),
+    path('remove_tags/<int:query_id>/', views.remove_tags_from_userquery, name='remove_tags_from_userquery'),
+    path('remove_query/<int:query_id>/', views.remove_userquery, name='remove_userquery'),
+    path('get_queries/<int:user_id>/', views.get_userqueries, name='get_userqueries'),
+    path('tags/', views.get_all_tags, name='get_all_tags'),
+    path('submit_guess/', views.submit_guess, name='submit_guess'),
+
 ]
