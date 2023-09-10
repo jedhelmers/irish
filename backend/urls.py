81bcebf9263d477a4fa15d5aa5c3c5805cf2006d
Jed Helmers <jedhelmers@gmail.com>
2023-09-09 23:23:00 -0400

    urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from my_app import views
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('metrics/', include('django_prometheus.urls')),
-    path('', include('my_app.urls')),
+    # path('', include('my_app.urls')),
+    path('', views.CustomTemplateView.as_view(), name='home'),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
