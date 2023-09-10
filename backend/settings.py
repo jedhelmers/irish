00f97988eead7b358f628d4150fe8a6185f8c6cf
Jed Helmers <jedhelmers@gmail.com>
2023-09-10 15:21:02 -0400

    debug

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from .celery import app as celery_app
 from pathlib import Path
 import os
+from decouple import config
 
 
 # Make sure it gets loaded when Django starts
@@ -31,8 +32,9 @@ DISABLE_COLLECTSTATIC = 1
 SECRET_KEY = 'django-insecure-1tr_r#8q+9!te@7m*b3&8h0#yca6aa!1h*u83lp9soee5iha*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-DEBUG = False
+# DEBUG = True
+# DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = ['backend', 'my_app', 'my_app_1', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
